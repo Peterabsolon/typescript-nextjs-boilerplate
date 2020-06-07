eb2b02bbb03c51cf3a24af1d94ce685ab7dbb0f5
Peter Absolon <peter@pex.com>
2020-06-07 22:47:01 +0200

    Test CI

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <meta name='description' content='Track your efforts'></meta>
         </Head>
         <body>
-          Document
+          Document (CI)
           <Main />
           <NextScript />
         </body>
