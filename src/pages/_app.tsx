7e23456fecddb9bc1b07686551fa94c75ff77d7f
Peter Absolon <peter@pex.com>
2020-06-07 23:19:21 +0200

    Add perf debug

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+// Perf debug
+// export function reportWebVitals(metric: any) {
+//   console.log(metric)
+// }
+
+export default function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />
+}
